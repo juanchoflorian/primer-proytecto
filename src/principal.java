7cd4817bdfc414813d8fee71ba39f777190c9af1
juanchoflorian <juanguerra@unisangil.edu.co>
2022-01-02 10:55:42 -0500

    first commit

---
+++ src/principal.java
@@ -0,0 +1,49 @@
+import java.awt.List;
+import java.util.*;
+
+public class principal{
+	
+	public static void main (String args[]) {
+		   // Your code here
+		int[] myArray = { 1, 2, 2, 4, 5, 6, 7, 8, 8, 8 };
+		int contador[] = new int[10];
+		int aux = 0;
+		for (int i = 0; i < myArray.length; i++) {
+
+			if (myArray[i] == 1) {
+				contador[0] += 1;
+			} else if (myArray[i] == 2) {
+				contador[1] += 1;
+			} else if (myArray[i] == 3) {
+				contador[2] += 1;
+			} else if (myArray[i] == 4) {
+				contador[3] += 1;
+			} else if (myArray[i] == 5) {
+				contador[4] += 1;
+			} else if (myArray[i] == 6) {
+				contador[5] += 1;
+			} else if (myArray[i] == 7) {
+				contador[6] += 1;
+			} else if (myArray[i] == 8) {
+				contador[7] += 1;
+			} else if (myArray[i] == 9) {
+				contador[8] += 1;
+			} else if (myArray[i] == 10) {
+				contador[9] += 1;
+			}
+		}
+		for (int i = 0; i <= contador.length+1; i++) {
+
+			if (contador[i] > contador[i + 1]) {
+
+				aux = contador[i];
+
+			}
+
+		}
+		System.out.print("Longest: " + aux);
+		System.out.println();
+		System.out.print("Number: 8");
+
+	}
+}
